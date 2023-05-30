f2435a5e5779e76ec147ebe5db3fa501b0b9d9b9
Lucas Solari <solarilucas92@gmail.com>
2023-05-30 18:42:37 +0000

    inicio proyecto

---
+++ src/js/component/list.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const Mylist = () => {
+  const [] = useState []
+  const [input, setInput] = useState("");
+
+  const handleInput = (e) => {}
+  return (
+
+    <div>
+        <h1>hola</h1>
+    </div>
+  );
+};
+
+
+export default Mylist;
