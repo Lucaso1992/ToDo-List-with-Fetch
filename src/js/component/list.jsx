520a81a62e026af82a004fd59b3c31a93ab7c589
Lucas Solari <solarilucas92@gmail.com>
2023-06-01 19:02:58 +0000

    modificaciones

--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -1,21 +1,30 @@
 import React, { useState } from "react";
 
 const Mylist = () => {
-  const [input, setInput] = useState([]);
-  const validateInput = () => {
-    if (input === input.length) useState.push(input);
-  };
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
   return (
     <div className="d-flex flex-column align-items-center">
       <h1>ToDo-List</h1>
-      <input
-        type="text"
-        placeholder="Escribe aquí..."
-        onChange={(e) => setInput(e.target.value)}
-        value={input}
-        onKeyDown={validateInput}
-      />
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat(inputValue));
+              }
+            }}
+            placeholder="What to do now?"
+          ></input>
+        </li>
+        {todos.map((t) => (
+          <li>{t}</li>
+        ))}
+      </ul>
       <span>
         <button>Press Enter</button>
       </span>
